1a111fc4d544f4ab8da8cc24a8d9f38955be6142
Steven Douglas <steven@douglas-enterprises.com>
2017-06-24 15:38:25 -0700

    Add script to automate poudriere runs

---
+++ poudriere_run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+PORTS_TREES='upstream'
+JAILS='FreeBSD:10:amd64 FreeBSD:11:amd64'
+PYTHON_VERSIONS='2.7 3.4 3.5 3.6'
+for PORTS_TREE in ${PORTS_TREES}; do
+
+        # Update poudriere ports tree
+        poudriere ports -u -p ${PORTS_TREE}
+
+        for JAIL in $JAILS; do
+                MAKEFILE="/usr/local/etc/poudriere.d/${JAIL}-make.conf"
+                # Ensure the jail is up to date
+                poudriere jail -u -j ${JAIL}
+
+                # Build the ports
+                poudriere bulk -f /usr/local/poudriere/pkg-list.txt -j ${JAIL} -p ${PORTS_TREE}
+
+                for PYTHON_VERSION in ${PYTHON_VERSIONS}; do
+                        echo "DEFAULT_VERSIONS+= python=${PYTHON_VERSION}" > ${MAKEFILE}
+                        poudriere bulk -f /usr/local/poudriere/pkg-list.txt -j ${JAIL} -p ${PORTS_TREE}
+                        rm -f ${MAKEFILE}
+                done
+        done
+
+done
